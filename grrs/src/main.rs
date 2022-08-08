02240bd55529adf1cefdc06560e5f67c392f8401
boapi <tobias.weyer1205@gmail.com>
2022-08-08 14:25:58 +0200

    update comments and structure

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -4,13 +4,22 @@ struct Cli {
 }
 
 fn main() {
-    println!("Hello, world!");
+    // say hell to the user
+    println!("Hello, user");
 
+    // parse cli argumrnent
+    // 1st argument - text pattern to look for
+    // 2nd argument - path to file, where the test pattern is used
     let pattern = std::env::args().nth(1).expect("no pattern given");
     let path = std::env::args().nth(2). expect("no path given");
 
+    // print both received arguments to the stdout
     println!("{pattern}");
     println!("{path}");
 
-
+   // putting the received argurments in variable of type Cli
+   let args = Cli {
+    pattern: pattern,
+    path: std::path::PathBuf::from(path),
+   };
 }
