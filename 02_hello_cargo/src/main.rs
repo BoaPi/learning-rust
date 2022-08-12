8db48338b5c26e35d5439bc44d5e4512b83d5d1d
boapi <tobias.weyer1205@gmail.com>
2022-08-12 16:24:15 +0200

    add user input

--- 02_hello_cargo/src/main.rs
+++ 02_hello_cargo/src/main.rs
@@ -1,3 +1,17 @@
+#![allow(unused)]
+
+use std::io;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+
 fn main() {
-    println!("Hello, cargo!");
+    println!("What is your name?");
+
+    let mut name = String::new();
+    let greeting: &str = "Nice to meet you";
+
+    io::stdin().read_line(&mut name)
+        .expect("Did not receive input");
+
+    println!("Hello {}!, {}", name.trim_end(), greeting);
 }
