34cada1936676d7508e6f5d035c371552f216fc9
boapi <tobias.weyer1205@gmail.com>
2022-08-23 22:24:20 +0200

    add test case for find_matches function

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -44,6 +44,8 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+// function which receives a BufReader and patternn to look for in 
+// each line of the BufReader
 fn find_matches<R: BufRead>(reader: R, pattern: &str) -> String {
     // print each line of file which contains the pattern
     let mut check: String;
@@ -66,7 +68,24 @@ mod tests {
     use super::*;
     
     #[test]
-    fn my_first_unit_test() {
-        assert_eq!(answer(), 42);
+    fn test_find_matches() {
+        // arrange
+        // generate byte stream from string
+        // create new BufReader and pass source
+        // create pattern string
+        let source = "Lorem ipsum dolor\nipsum\ndolor some stuff".as_bytes();
+        let reader = BufReader::new(source);
+        let pattern: &str = "dolor";
+
+        // act
+        // call function to test
+        // pass reader and pattern to function
+        let result: String = find_matches(reader, pattern);
+
+        // assert
+        // create target match
+        // compare result with target match
+        let matches: String = String::from("Lorem ipsum dolor\ndolor some stuff\n");
+        assert_eq!(result, matches);
     }
 }
