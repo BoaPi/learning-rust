77a24ec9f65fd01d2f843aa1ab79332af3a99eaf
boapi <tobias.weyer1205@gmail.com>
2022-08-29 22:52:03 +0200

    refactor to use lib function

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::{Context, Result};
 use clap::Parser;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader, Write};
+use std::io::{self, BufReader, Write};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -34,7 +34,7 @@ fn main() -> Result<()> {
     let reader = BufReader::new(content);
 
     // check reader content for matching pattern
-    let findings: String = find_matches( reader, &args.pattern);
+    let findings: String = grrs::find_matches( reader, &args.pattern);
         
     // print result
     writeln!(handle, "{}", findings)?;
@@ -44,48 +44,3 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-// function which receives a BufReader and patternn to look for in 
-// each line of the BufReader
-fn find_matches<R: BufRead>(reader: R, pattern: &str) -> String {
-    // print each line of file which contains the pattern
-    let mut check: String;
-    let mut result: String = String::from("");
-
-    for line in reader.lines() {
-        check = line.unwrap();
-
-        if check.contains(pattern) {
-            result.push_str(&check);
-            result.push_str("\n");
-        }
-    }
-
-    return result;
-} 
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    
-    #[test]
-    fn test_find_matches() {
-        // arrange
-        // generate byte stream from string
-        // create new BufReader and pass source
-        // create pattern string
-        let source = "Lorem ipsum dolor\nipsum\ndolor some stuff".as_bytes();
-        let reader = BufReader::new(source);
-        let pattern: &str = "dolor";
-
-        // act
-        // call function to test
-        // pass reader and pattern to function
-        let result: String = find_matches(reader, pattern);
-
-        // assert
-        // create target match
-        // compare result with target match
-        let matches: String = String::from("Lorem ipsum dolor\ndolor some stuff\n");
-        assert_eq!(result, matches);
-    }
-}
