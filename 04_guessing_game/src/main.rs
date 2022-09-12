6798896bb48e3b433d92ae3dfdbc3d3458296d9a
boapi <tobias.weyer1205@gmail.com>
2022-09-12 23:40:58 +0200

    refactor to use new lib functions

--- 04_guessing_game/src/main.rs
+++ 04_guessing_game/src/main.rs
@@ -1,30 +1,10 @@
-use std::io;
-
 fn main() {
     // greetings for the user
-    greeting();
+    guessing_game::greeting();
 
     // create variable for the user guess
     let guess: String;
-    guess = process_input();
+    guess = guessing_game::process_input();
 
     println!("You guessed: {guess}");
-}
-
-// function which greets the user
-// and explains the user interactions
-fn greeting() {
-    println!("Guess the number!");
-    println!("Please input your guess");
-}
-
-// function to process the user input
-// and writes input into guess variable
-fn process_input() -> String {
-    let mut guess: String = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    guess
 }
