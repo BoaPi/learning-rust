8b5f4a7c75929ad6f7f2ab3586ad34d0d20b1095
boapi <tobias.weyer1205@gmail.com>
2022-09-12 23:24:51 +0200

    refactor main to separate functions

--- 04_guessing_game/src/main.rs
+++ 04_guessing_game/src/main.rs
@@ -1,15 +1,28 @@
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    // greetings for the user
+    greeting();
 
-    println!("Please input your guess");
-
+    // create mutuable variable for the user guess
     let mut guess: String = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    process_input(&mut guess);
 
     println!("You guessed: {guess}");
+}
+
+// function which greets the user
+// and explains the user interactions
+fn greeting() {
+    println!("Guess the number!");
+    println!("Please input your guess");
+}
+
+// function to process the user input
+// and writes input into guess variable
+fn process_input(guess: &mut String) {
+    io::stdin()
+        .read_line(guess)
+        .expect("Failed to read line");
 }
