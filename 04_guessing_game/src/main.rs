1286e4a24220cebe6c9c3ad52a134d637464a4e3
boapi <tobias.weyer1205@gmail.com>
2022-09-15 22:11:54 +0200

    remove user input enum type

--- 04_guessing_game/src/main.rs
+++ 04_guessing_game/src/main.rs
@@ -3,13 +3,13 @@ fn main() {
     guessing_game::greeting();
 
     // create variable for the user guess
-    let guess: Result<guessing_game::UserInput, String>;
+    let guess: Result<String, String>;
 
     // guess will be one of three variants
     guess = guessing_game::process_input();
 
     match guess {
-        Ok(value) => println!("your guess is {:?}", value),
-        Err(error) => println!("ERROR: {:?}", error),
+        Ok(value) => println!("your guess is {}", value),
+        Err(error) => println!("ERROR: {}", error),
     }
 }
