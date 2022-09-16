478c469bad44112789695a06a4d802623a9a73d5
boapi <tobias.weyer1205@gmail.com>
2022-09-16 16:49:25 +0200

    add compare function which returns a Result

--- 04_guessing_game/src/lib.rs
+++ 04_guessing_game/src/lib.rs
@@ -29,3 +29,13 @@ pub fn process_input() -> Result<String, String> {
 pub fn generate_secret_number() -> i32 {
     rand::thread_rng().gen_range(1..=10)
 }
+
+// compare two numbers
+// return to low, to high or equal message
+pub fn compare_numbers(guess: i32, secret: i32) -> Result<String, String> {
+    match guess.cmp(&secret) {
+        Ordering::Less => Err("To Small".to_string()),
+        Ordering::Greater => Err("To Big".to_string()),
+        Ordering::Equal => Ok("You Win".to_string()),
+    } 
+}
