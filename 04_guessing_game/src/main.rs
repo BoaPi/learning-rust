7f7800a33c6f53199b4a56e12acdde46a393c0d3
boapi <tobias.weyer1205@gmail.com>
2022-09-17 22:07:19 +0200

    add loop for multiple guess tries

--- 04_guessing_game/src/main.rs
+++ 04_guessing_game/src/main.rs
@@ -5,24 +5,27 @@ fn main() {
     guessing_game::greeting();
 
     // create variable for the user guess
-    let guess: Result<i32, ParseIntError>;
-
-    // guess will be one of three variants
-    guess = guessing_game::process_input();
+    let mut guess: Result<i32, ParseIntError>;
 
     // generate secret number
     let secret_number = guessing_game::generate_secret_number();
 
-    match guess {
-        Ok(value) => {
-           println!("your guess is {}", value);
+    // creating a loop, to get as much guesses as needed
+    loop {
+        // guess will be one of three variants
+        guess = guessing_game::process_input();
 
-           // give hint about the guess
-           match guessing_game::compare_numbers(value, secret_number) {
-               Ok(message) => println!("{}", message),
-               Err(error) => eprintln!("GUESS WRONG: {}", error),
-           }
-        },
-        Err(error) => eprintln!("ERROR: {}", error),
+        match guess {
+            Ok(value) => {
+                println!("your guess is {}", value);
+
+                // give hint about the guess
+                match guessing_game::compare_numbers(value, secret_number) {
+                    Ok(message) => {println!("{}", message)},
+                    Err(error) => eprintln!("GUESS WRONG: {}", error),
+                }
+            }
+            Err(error) => eprintln!("ERROR: {}", error),
+        }
     }
 }
