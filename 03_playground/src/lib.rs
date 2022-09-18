fa1d39aa5301f7d04c23d36e2188a00da81ec4a5
boapi <tobias.weyer1205@gmail.com>
2022-09-18 22:46:29 +0200

    add lib file for different lessons to be encapsulated as functions

---
+++ 03_playground/src/lib.rs
@@ -0,0 +1,33 @@
+// custom enum lesson
+// ip address enum examples
+#[derive(Debug)]
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+// custom method for enum IpAddr
+impl IpAddr {
+    fn call(&self) {
+        println!("{:?}", self)
+    }
+}
+
+pub fn custom_enum_lesson() {
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    home.call();
+    loopback.call();
+}
+
+// immutability/ mutability lesson
+pub fn immutability_lesson() {
+    // next line wont compile, because of immutability
+    // let x: i32 = 5;
+    let mut x: i32 = 5;
+    println!("{}", x);
+
+    x = 6;
+    println!("{}", x);
+}
