28a8d6b07e9dd1cc109da001644e8e42904cfb3b
boapi <tobias.weyer1205@gmail.com>
2022-09-21 22:54:07 +0200

    apply formatting

--- 03_playground/src/main.rs
+++ 03_playground/src/main.rs
@@ -1,7 +1,6 @@
 // individual lesson are written in the lin.rs file
 fn main() {
-   playground::custom_enum_lesson();
-   playground::immutability_lesson();
-   playground::shadowing_lesson();
+    playground::custom_enum_lesson();
+    playground::immutability_lesson();
+    playground::shadowing_lesson();
 }
-
