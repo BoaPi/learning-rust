2cf255ca05ac5fb31d1aaa5ce97fae8849f3a2fd
boapi <tobias.weyer1205@gmail.com>
2022-09-24 23:07:10 +0200

    add import everything from playground lib add simple_data_type lesson

--- 03_playground/src/main.rs
+++ 03_playground/src/main.rs
@@ -1,6 +1,9 @@
+use playground::*;
+
 // individual lesson are written in the lin.rs file
 fn main() {
     playground::custom_enum_lesson();
     playground::immutability_lesson();
     playground::shadowing_lesson();
+    simple_data_type();
 }
