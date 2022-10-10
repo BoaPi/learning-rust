9c9e669e59e4402bd432d79e993ee45bf0016777
boapi <tobias.weyer1205@gmail.com>
2022-10-10 22:46:06 +0200

    add proper commenting

--- 05_tiktok_game/src/board.rs
+++ 05_tiktok_game/src/board.rs
@@ -29,12 +29,21 @@ impl Board {
         };
     }
 
+    // (A)
+    // get position values
+    // get current piece type of the board in given position
+    //
+    // (B)
+    // if current piece type is empty return false to indicate position on 
+    // board is already taken
     pub fn submit(&mut self, player: u8, pos: Position, piece_type: PieceType) -> bool {
+        // (A)
         let (x, y) = pos;
         let piece = self.board[y as usize][x as usize];
 
+        // (B)
         if piece != PieceType::EmptyPiece {
-            return false
+            return false;
         }
     }
 }
