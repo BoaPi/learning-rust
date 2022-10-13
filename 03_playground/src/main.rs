d028483426bcf28d6653029be52a257c92ec65d5
boapi <tobias.weyer1205@gmail.com>
2022-10-13 22:40:42 +0200

    reorder lessons to have newest one always on top

--- 03_playground/src/main.rs
+++ 03_playground/src/main.rs
@@ -2,18 +2,18 @@ use playground::*;
 
 // individual lesson are written in the lin.rs file
 fn main() {
-    custom_enum_lesson();
-    immutability_lesson();
-    shadowing_lesson();
-    simple_data_type_lesson();
-    basic_numeric_operations_lesson();
-    basic_boolean_lesson();
-    basic_character_lesson();
-    basic_tuple_lesson();
-    basic_array_lesson();
-    extended_array_lesson();
-    basic_function_lesson();
-    basic_loop_lesson();
-    nesting_loops_lesson();
     while_loop_lesson();
+    nesting_loops_lesson();
+    basic_loop_lesson();
+    basic_function_lesson();
+    extended_array_lesson();
+    basic_array_lesson();
+    basic_tuple_lesson();
+    basic_character_lesson();
+    basic_boolean_lesson();
+    basic_numeric_operations_lesson();
+    simple_data_type_lesson();
+    shadowing_lesson();
+    immutability_lesson();
+    custom_enum_lesson();
 }
