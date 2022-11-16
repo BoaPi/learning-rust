82f16a35ef559634cd6b96eb9ed4cb6ad746c2cd
boapi <tobias.weyer1205@gmail.com>
2022-11-16 22:26:03 +0100

    add basic_struct_lesson()

--- 03_playground/src/main.rs
+++ 03_playground/src/main.rs
@@ -2,6 +2,7 @@ use playground::*;
 
 // individual lesson are written in the lin.rs file
 fn main() {
+    basic_struct_lesson();
     second_slice_lesson();
     first_slice_lesson();
     ownership_lesson();
