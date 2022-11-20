c863b5af48195d093d2d4c12d47c975505ac8144
boapi <tobias.weyer1205@gmail.com>
2022-11-20 22:24:46 +0100

    add tupple_structs_lesson()

--- 03_playground/src/main.rs
+++ 03_playground/src/main.rs
@@ -2,6 +2,7 @@ use playground::*;
 
 // individual lesson are written in the lin.rs file
 fn main() {
+    tuple_structs_lesson();
     building_structs_lesson();
     basic_struct_lesson();
     second_slice_lesson();
