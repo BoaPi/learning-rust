0a33f6310d5bf6b11f9ceffe9f3c4a8893dae1eb
boapi <tobias.weyer1205@gmail.com>
2023-01-01 21:46:59 +0100

    add match lesson

--- 03_playground/src/main.rs
+++ 03_playground/src/main.rs
@@ -2,6 +2,7 @@ use playground::*;
 
 // individual lesson are written in the lib.rs file
 fn main() {
+    match_lesson();
     enum_lesson();
     rectangle_lesson();
     unit_like_structs_lesson();
